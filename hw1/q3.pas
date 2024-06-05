7bf04a8733f0231fe8dcde25e77149543a2932cc
Ronvelitsky <94012697+Ronvelitsky@users.noreply.github.com>
2024-06-05 23:16:20 +0300

    Update q3.pas

--- hw1/q3.pas
+++ hw1/q3.pas
@@ -1,24 +1,23 @@
 program ThirdQuestion;
 type
-    CourseType: (REG, LAB, SEM);
+    CourseType = (REG, LAB, SEM);
 
 
-type Course = record
+    Course = record
     typeOfCourse: CourseType;
     Id : Integer;
     name : string;
-    case typeOfCourse : CourseType of
-        REG : (numberOfLectures: Integer)
-        LAB : (nameOfLecturer: string, place: string)
-        SEM : (startTime, endTime: string)
+    case data : CourseType of
+        REG : (numberOfLectures: Integer);
+        LAB : (nameOfLecturer: string; place: string);
+        SEM : (startTime: string; endTime: string)
     end;
-    Course_arr = array[0..99] of Course;
-    Course_set = set of 0..99;
+Course_arr = array[0..99] of Course;
 var
     courses : Course_arr;
-    courses_exist : Course_set; 
     flag : boolean;
-    coursetype : CourseType;
+    coursetypee : string;
+    typee : CourseType;
     id : Integer;
     name: string;
     data1: Integer;
@@ -26,9 +25,9 @@ var
     data3: string;
     action: string;   
 
-procedure add(CourseType tp, Integer id, string name,Integer data1, string data2, sting data3)
+procedure add(var tp:CourseType; var id:Integer; var name:string;var data1:Integer; var data2:string; var data3:string);
 begin
-    courses[id-1000].typeOfCourse := REG;
+    courses[id-1000].typeOfCourse := tp;
     courses[id-1000].Id := id;
     courses[id-1000].name := name;
     case tp of
@@ -39,53 +38,50 @@ begin
         LAB:
         begin
             courses[id-1000].nameOfLecturer := data2;
-            courses[id-1000].nameOfLecturer := data3;
+            courses[id-1000].place := data3;
         end;
         SEM:
         begin
             courses[id-1000].startTime := data2;
             courses[id-1000].endTime := data3;
         end;
-    //courses_exist := courses_exist + [id];
 end;
 
 begin
-flag := true;
-while flag do
-begin
-    ReadLn(action);
-    case action of
-    "ADD":
-        begin
-            ReadLn(coursetype);
-            ReadLn(id);
-            ReadLn(name);
-                case coursetype of
-                "REG":
-                    begin
-                        ReadLn(data1);
-                        add(REG,id,data1,"buffer","buffer")
-                    end;
-                "LAB":
-                    begin
-                        ReadLn(data2);
-                        ReadLn(data3);
-                        add(LAB,id,"buffer",data2,data3);
-                    end;
-                "SEM":
-                    begin
-                        ReadLn(data2);
-                        ReadLn(data3);
-                        add(SEM,id,"buffer",data2,data3);
-                    end;
-        end;
-    "PRINT":
-        // Dariaa your code here
-        ;
-    "END": flag:=false;
+    flag := true;
+    while flag do
+    begin
+        ReadLn(action);
+        if action = 'ADD' then 
+            begin
+                ReadLn(coursetypee);
+                ReadLn(id);
+                ReadLn(name);
+                    if coursetypee = 'REG' then
+                        begin
+                            ReadLn(data1);
+                            typee := REG;
+                            add(typee,id,name,data1,'buffer','buffer');
+                        end;
+                    if coursetypee = 'LAB' then
+                        begin
+                            ReadLn(data2);
+                            ReadLn(data3);
+                            typee := LAB;
+                            add(typee,id,name,1,data2,data3);
+                        end;
+                    if coursetypee = 'SEM' then
+                        begin
+                            ReadLn(data2);
+                            ReadLn(data3);
+                            typee := SEM;
+                            add(typee,id,name,1,data2,data3);
+                        end;
+            end;
+        if action = 'PRINT' then
+            // dariaa your code here
+            ;
+        if action = 'END' then flag:=false;
 
-end;
-
-
-
+    end;
 end.
